01ff824dcd17af754203d1a306a264a6ad29162a
krupzz09 <161936717+krupzz09@users.noreply.github.com>
2024-03-02 04:22:03 -0900

    Add files via upload

---
+++ m.js
@@ -0,0 +1,9 @@
+/**
+ * Js event handler to change between Clark and Superman modes.
+ * 
+ */
+
+$('.btn').on('click', function() {
+  $('#target').toggleClass('superman');
+  $(this).toggleClass('active');
+});
